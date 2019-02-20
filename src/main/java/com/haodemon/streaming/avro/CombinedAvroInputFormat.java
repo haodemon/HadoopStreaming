aff247cdac6ff69e4a4df858ad8b6d1cfdffb54c
haodemon <haodemon@gmail.com>
2019-02-20 17:47:39 +0300

    Added CustomAvroAsTextInputFormat to read avro with invalid sync data

--- src/main/java/com/haodemon/streaming/avro/CombinedAvroInputFormat.java
+++ src/main/java/com/haodemon/streaming/avro/CombinedAvroInputFormat.java
@@ -1,5 +1,6 @@
 package com.haodemon.streaming.avro;
 
+import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.mapred.AvroAsTextInputFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
@@ -22,7 +23,57 @@ public class CombinedAvroInputFormat extends CombineFileInputFormat<Text, Text> 
                                       Configuration conf,
                                       Reporter reporter,
                                       Integer idx) throws IOException {
-            super(new AvroAsTextInputFormat(), split, conf, reporter, idx);
+            super(new CustomAvroAsTextInputFormat(), split, conf, reporter, idx);
+        }
+    }
+
+    /**
+     * Wrapped to avoid exceptions for avro with invalid sync data
+     */
+    private static class CustomAvroAsTextInputFormat extends AvroAsTextInputFormat {
+        private static final String EXCEPTION_INVALID_SYNC = "java.io.IOException: Invalid sync!";
+        @Override
+        public RecordReader<Text, Text> getRecordReader(InputSplit split,
+                                                        JobConf job,
+                                                        Reporter reporter) throws IOException {
+            RecordReader<Text, Text> rr = super.getRecordReader(split, job, reporter);
+            return new RecordReader<Text, Text>() {
+                @Override
+                public boolean next(Text key, Text ignore) throws IOException {
+                    try {
+                        return rr.next(key, ignore);
+                    }
+                    catch (AvroRuntimeException e) {
+                        if (e.getMessage().matches(EXCEPTION_INVALID_SYNC)) return false;
+                        throw e;
+                    }
+                }
+
+                @Override
+                public Text createKey() {
+                    return rr.createKey();
+                }
+
+                @Override
+                public Text createValue() {
+                    return rr.createValue();
+                }
+
+                @Override
+                public long getPos() throws IOException {
+                    return rr.getPos();
+                }
+
+                @Override
+                public void close() throws IOException {
+                    rr.close();
+                }
+
+                @Override
+                public float getProgress() throws IOException {
+                    return rr.getProgress();
+                }
+            };
         }
     }
 }
